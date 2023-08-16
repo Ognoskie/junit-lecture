c22cef47f3046b6fc539c2b167a12201ac386982
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-16 11:19:54 -0500

    added more to test

--- src/main/java/Coffee.java
+++ src/main/java/Coffee.java
@@ -1,4 +1,27 @@
 public class Coffee {
 
+	private String name;
+	private String roast;
+	private double price;
 
+	public Coffee () {}
+
+	public Coffee (String name, String roast, double price) {
+		this.name = name;
+		this.roast = roast;
+		this.price = price;
+	}
+
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getRoast() {
+		return this.roast;
+	}
+
+	public double getPrice() {
+		return price;
+	}
 }
--- src/test/java/CoffeeTest.java
+++ src/test/java/CoffeeTest.java
@@ -1,13 +1,42 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CoffeeTest {
-
+    private double deltaValue = 0.000001;
 	private Coffee emptyCoffee;
 	private Coffee actualCoffee;
 
-	@Test
-	public void	testCoffeeConstructors() {
-		actualCoffee = new Coffee();
+	@Before
+	public void setup() {
+		actualCoffee = new Coffee("The Bean-i-nator", "espresso", 5.99);
 	}
+
+	@Test
+	public void	testCoffeeNoArgConstructors() {
+		actualCoffee = new Coffee();
+
+		// assert that emptyCoffee is null
+		assertNull(emptyCoffee);
+
+		// assert that actualCoffee is not null
+		assertNotNull(actualCoffee);
+	}
+
+	@Test
+	public void testCoffeeThreeArgConstructors() {
+		assertNotNull(actualCoffee);
+	}
+
+	@Test
+	public void	testCoffeeGetters() {
+		assertEquals(actualCoffee.getName(), "The Bean-i-nator");
+		assertEquals(actualCoffee.getRoast(), "espresso");
+		assertEquals(actualCoffee.getPrice(), 5.99, deltaValue);
+	}
+
+
+
+
+
 }
